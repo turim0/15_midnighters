4b13e0046780299dfbdc454bdc719eaac145fd57
turim0 <ivan_medvedev_1996@mail.ru>
2019-02-28 16:25:22 +0300

    first version

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Displays all  Devman's users who send task for review between 0:00 and 6:00 a.m.  
 
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python seek_dev_nighters.py  # possibly requires call of python3 executive instead of just python
+idmedvedevivan                12/02/19 01:05
+idmedvedevivan                12/02/19 00:50
+idmedvedevivan                10/02/19 00:31
+m.chernyshov                  07/02/19 00:48
+m.chernyshov                  07/02/19 00:42
+m.chernyshov                  07/02/19 00:39
+...
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,28 @@
+import requests
+from datetime import datetime
+from pytz import timezone
+
+
 def load_attempts():
-    pages = 1
+    pages = 10
     for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+        response = requests.get(
+            'http://devman.org/api/challenges/solution_attempts/?page='+str(page+1)
+        )
+        decoded_json = response.json()
+        for record in decoded_json['records']:
+            yield record
+
 
 def get_midnighters():
-    pass
+    for record in load_attempts():
+        tz = timezone(record['timezone'])
+        post_time = datetime.fromtimestamp(record['timestamp'], tz=tz)
+        if post_time.hour in range(7):
+            username = record['username']
+            post_time = post_time.strftime('%d/%m/%y %H:%M')
+            print('{:<30}{:14}'.format(username, post_time))
+
 
 if __name__ == '__main__':
-  pass
+    get_midnighters()
