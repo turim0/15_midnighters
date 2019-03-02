263cda6524db6017e21e1542317b11818b2b135a
turim0 <ivan_medvedev_1996@mail.ru>
2019-03-02 15:19:22 +0300

    fix func

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -13,19 +13,18 @@ def load_attempts():
             break
         decoded_json = response.json()
         page += 1
-        for record in decoded_json['records']:
-            yield record
+        for attempt in decoded_json['records']:
+            yield attempt
 
 
-def get_midnighters():
+def get_midnighters(attempt):
     midnighters = defaultdict(list)
-    for record in load_attempts():
-        tz = timezone(record['timezone'])
-        post_time = datetime.fromtimestamp(record['timestamp'], tz=tz)
-        if post_time.hour in range(7):
-            username = record['username']
-            post_time = post_time.strftime('%d/%m/%y %H:%M')
-            midnighters[username].append(post_time)
+    tz = timezone(attempt['timezone'])
+    post_time = datetime.fromtimestamp(attempt['timestamp'], tz=tz)
+    if post_time.hour in range(7):
+        username = attempt['username']
+        post_time = post_time.strftime('%d/%m/%y %H:%M')
+        midnighters[username].append(post_time)
     return midnighters
 
 
@@ -37,5 +36,6 @@ def pprint(midnighters):
 
 
 if __name__ == '__main__':
-    midnighters = get_midnighters()
-    pprint(midnighters)
+    for attemp in load_attempts():
+        midnighters = get_midnighters(attemp)
+        pprint(midnighters)
